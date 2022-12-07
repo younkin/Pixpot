0404bff6241690581015e0215da468b4e36e96e3
younkin <unkin.evgenii@gmail.com>
2022-12-07 19:48:32 +0400

    Initial Commit

---
+++ TaskEffectiveMobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskEffectiveMobile
+//
+//  Created by Евгений Юнкин on 07.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
